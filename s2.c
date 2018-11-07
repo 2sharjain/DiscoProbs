5d17c75ba0e7618c65727b461b5a4c3222b11d9a
2sharjain <93toshit@gmail.com>
2018-11-08 03:15:01 +0530

    v2

--- s2.c
+++ s2.c
@@ -4,14 +4,10 @@ int findMinCost(int arr[20][20], int n);
 
 int main(){
 
-
     int n, m, cost[20][20];
 
-    for(int i=0; i<20; i++){
-        for(int j=0; j<20; j++){
-            cost[i][j]=0;
-        }
-    }
+    for(int i=0; i<20; i++) for(int j=0; j<20; j++)  cost[i][j]=0;
+           
 
     scanf(" %d",&n);
     scanf(" %d",&m);
@@ -28,8 +24,8 @@ int main(){
 
 int findMinCost(int arr[20][20], int n){
     int count=0, city=0, min=1000, minCost=0, visited[20];
-    for(int q=0; q<n; q++) visited[q]=-1;
-    while(count==n){
+    for(int q=0; q<20; q++) visited[q]=-1;
+    while(count==n+1){
         visited[city]=0;
         for(int j=0; j<n; j++){
             if (arr[city][j]==0||visited[j]==0) continue;
@@ -44,9 +40,12 @@ int findMinCost(int arr[20][20], int n){
                 minCost+=min;
                 city=k;
                 min=1000;
+                break;
             }
+            else continue;
         }
-        if(count==n-1){
+        
+        if(count==n){
             minCost+=arr[city][0];
             break;
         }
